15084abed2c16939f0cf377744954b434a92c1fb
anuplohar001 <anuplohar001@gmail.com>
2024-09-20 22:53:24 +0530

    add: corsOptions

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ configDotenv()
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "http://prompts-book.vercel.app",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
