b849caae9a137c2624f9e70fedd0ab21feecb9bf
anuplohar001 <anuplohar001@gmail.com>
2024-12-25 19:13:18 +0530

    add: websockets

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose';
 import cors from 'cors'
 import { configDotenv } from 'dotenv';
-import Message from '../model/message.js';
-import apiEndpoints from '../endpoints/APIEndpoints.js';
+import Message from './model/message.js';
+import apiEndpoints from './endpoints/APIEndpoints.js';
 import http from "http";
 import { Server } from "socket.io";
 import moment from 'moment-timezone'
